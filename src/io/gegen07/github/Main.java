1fdb9128578c6ff2b05e16f34c373456c9f43f0f
Germano Barcelos <germanobarcelos@yahoo.com.br>
2018-11-20 15:41:32 -0200

    4 Servers finished

--- src/io/gegen07/github/Main.java
+++ src/io/gegen07/github/Main.java
@@ -1,8 +1,23 @@
 package io.gegen07.github;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        int port = 12000;
+
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server(port));
+        servers.add(new Server(port+1));
+        servers.add(new Server(port+2));
+        servers.add(new Server(port+3));
+
+        for (Server s: servers) {
+            s.startServer();
+        }
     }
 }
--- src/io/gegen07/github/Server.java
+++ src/io/gegen07/github/Server.java
@@ -14,28 +14,19 @@ import java.util.List;
 import java.util.Map;
 
 public class Server {
-    private static PrintWriter out;
-    private static BufferedReader in;
-    private static List<Client> clientList;
+    private PrintWriter out;
+    private BufferedReader in;
+    private List<Client> clientList;
+    private ServerSocket serverSocket;
 
-    public static void main(String[] args) throws IOException {
-        List<ServerSocket> serverSocket = new ArrayList<>();
-        serverSocket.add(new ServerSocket(12000));
-        serverSocket.add(new ServerSocket(12001));
-        serverSocket.add(new ServerSocket(12002));
-        serverSocket.add(new ServerSocket(12003));
-
-        for (ServerSocket s: serverSocket) {
-            startServer(s);
-        }
+    public Server (int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientList = new ArrayList<>();
     }
-
-    public static void startServer(ServerSocket serverSocket) throws IOException {
-
-        List<Client> clientList = new ArrayList<>();
+    public void startServer() throws IOException {
 
         while (true) {
-            Client client = new Client(serverSocket.accept());
+            Client client = new Client(this.serverSocket.accept());
 
             out = new PrintWriter(client.getSocket().getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getSocket().getInputStream()));
@@ -45,12 +36,12 @@ public class Server {
             startCredentials(client);
 
             ClientHandler handler = new ClientHandler(client);
-            clientList.add(client);
+            this.clientList.add(client);
             handler.start();
         }
     }
 
-    public static void startCredentials(Client client) throws IOException {
+    public void startCredentials(Client client) throws IOException {
         out.println("Digite seu username: ");
         client.setUsername(in.readLine());
         out.println("Digite seu nome: ");
